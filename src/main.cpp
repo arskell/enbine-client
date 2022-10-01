fb72220e7e37d9f2632ad6b950d752c340d49216
arskell <arskell221@gmail.com>
2022-10-01 20:04:06 +0300

    client skeleton

---
+++ src/main.cpp
@@ -0,0 +1,56 @@
+
+#include <iostream>
+#include <stdio.h>
+#include <cassert>
+#include <GLFW/glfw3.h>
+#include <GL/gl.h>
+#include <array>
+#include <memory.h>
+
+void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+struct Color{
+    unsigned char r;
+    unsigned char g;
+    unsigned char b;
+} __attribute__((packed));
+
+int main(){
+    
+    assert(glfwInit());
+    glfwSetErrorCallback(error_callback);
+
+    GLFWwindow *wnd = glfwCreateWindow(500, 600, "Scene", nullptr, nullptr);
+    assert(wnd);
+
+    std::array<Color, 600*600> image;
+    memset(image.data(), 255, image.size() * sizeof(Color));
+
+    glfwMakeContextCurrent(wnd);
+    glfwSwapBuffers(wnd);
+
+    int counter  = 0;
+
+    glRasterPos2f(-1,1);
+    glPixelZoom( 1, -1 );
+
+    while(!glfwWindowShouldClose(wnd)){
+        glfwPollEvents();
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawPixels(600, 600, GL_RGB,  GL_UNSIGNED_BYTE, image.data());
+        
+        image[counter].r = 0;
+        image[counter].g = 0;
+        image[counter].b = 0;
+        counter = (counter + 1)%image.size();
+        glfwSwapBuffers(wnd);
+    }
+
+    glfwDestroyWindow(wnd);
+    glfwTerminate();   
+
+}
