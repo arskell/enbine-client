f77698e33f020306f4fdf0319471e4d57da8e65d
arskell <arskell221@gmail.com>
2022-10-15 00:04:18 +0300

    hello triangle

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,8 @@
 #include <array>
 #include <memory.h>
 
-#include <enbine/graphics/angle/euler_angles.h>
+#include "enbine/graphics/scene/view_port/view_port.h"
+#include "enbine/graphics/scene/primitives/triangle.h"
 
 void error_callback(int error, const char* description)
 {
@@ -22,13 +23,10 @@ struct Color{
 
 int main(){
     
-    EulerAngles a{},b{};
-    auto c = a + b;
-    
     assert(glfwInit());
     glfwSetErrorCallback(error_callback);
 
-    GLFWwindow *wnd = glfwCreateWindow(500, 600, "Scene", nullptr, nullptr);
+    GLFWwindow *wnd = glfwCreateWindow(600, 600, "Scene", nullptr, nullptr);
     assert(wnd);
 
     std::array<Color, 600*600> image;
@@ -42,16 +40,41 @@ int main(){
     glRasterPos2f(-1,1);
     glPixelZoom( 1, -1 );
 
+    ViewPort view_port;
+    Triangle triangle{
+        {-200, 0, 10},{200, 200 , 10},{0, 200, 10}
+        };
+
+    for(int x = 0; x < 600; ++x){
+        for(int y = 0; y < 600; ++y){
+            auto index = x + y*600;
+
+            auto ray = view_port.get_ray(x, y);
+
+            auto state = calc_intersection(triangle, ray);
+
+            if(state.is_intersected){
+                
+                image[index].r = 0;
+                image[index].g = 0;
+                image[index].b = 0;
+
+            }else{
+                image[index].r = 255;
+                image[index].g = 255;
+                image[index].b = 255;
+            }
+        }
+    }
+
     while(!glfwWindowShouldClose(wnd)){
         glfwPollEvents();
         glClear(GL_COLOR_BUFFER_BIT);
 
         glDrawPixels(600, 600, GL_RGB,  GL_UNSIGNED_BYTE, image.data());
         
-        image[counter].r = 0;
-        image[counter].g = 0;
-        image[counter].b = 0;
-        counter = (counter + 1)%image.size();
+
+        //counter = (counter + 1)%image.size();
         glfwSwapBuffers(wnd);
     }
 
