20ef8bcaa55fd998298853b198bae8fe298ddfe5
arskell <arskell221@gmail.com>
2022-12-21 00:36:37 +0300

    Demo update

--- src/main.cpp
+++ src/main.cpp
@@ -15,9 +15,13 @@
 #include "enbine/graphics/scene/primitives/triangle.h"
 #include "enbine/graphics/scene/scene.h"
 #include "enbine/graphics/scene/light/point_light.h"
+#include "enbine/graphics/scene/light/point_directional_light.h"
+
 #include "enbine/graphics/material/metal.h"
 
 #include "enbine/graphics/scene/primitives/square.h"
+#include <chrono>
+#include <thread>
 
 void error_callback(int error, const char* description)
 {
@@ -66,6 +70,7 @@ void do_post_processing_linear(const std::array<LightComponentT, Size>& in, std:
 void render(std::array<Color, 600*600>& image, float tick){
     
     static std::array<LightComponentT, 600 * 600> raw_image;
+    auto start =  std::chrono::high_resolution_clock::now();
 
     for(int x = 0; x < 600; ++x){
         for(int y = 0; y < 600; ++y){
@@ -81,6 +86,10 @@ void render(std::array<Color, 600*600>& image, float tick){
 
         }
     }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto dur = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    //std::cout<<dur.count()<< "ms" <<std::endl;
 
     do_post_processing_linear(raw_image, image);
 
@@ -135,25 +144,46 @@ int main(){
         {-500, 0 - 100, 300},{500, 200 - 100, 300},{0, 500 - 100, 300}, default_metal
     });
 
-    auto square = std::make_shared<Square>( Vec3{-100, -100,100}, Vec3{1,0,0}, Vec3{0,1,0}, 200.0f, 200.0f, default_metal);
+
+    auto floor = std::make_shared<Square>( Vec3{-3000, 10, -3000}, Vec3{1,0,0}, Vec3{0,0,1}, 6000.0f, 6000.0f, default_metal);
+
+
+    
+    auto wall_1 = std::make_shared<Square>( Vec3{-10, 10, 25}, Vec3{0.707f, 0, 0.707f}, Vec3{0,-1,0}, 10.0f, 2.0f, default_metal_2);
+    auto wall_2 = std::make_shared<Square>( Vec3{-10, 6, 25}, Vec3{0.707f, 0, 0.707f}, Vec3{0,-1,0}, 10.0f, 2.0f, default_metal_2);
+    auto wall_3 = std::make_shared<Square>( Vec3{-10, 2, 25}, Vec3{0.707f, 0, 0.707f}, Vec3{0,-1,0}, 10.0f, 2.0f, default_metal_2);
+
 
     auto square_front = std::make_shared<Square>( Vec3{-50, -50, 50}, Vec3{1,0,0}, Vec3{0,1,0}, 50.0f, 50.0f, default_metal_2);
 
 
     auto light = std::make_shared<PointLight>();
     light->set_flux({1, 1, 1});
-    light->set_position({0, 0, 0});
-    light->set_n({0,0,1});
+    light->set_position({0, 0, 22});
+    light->set_n({0,1,1});
 
-    scene.add("triangle", square);
-    scene.add("triangle_2", square_front);
+    auto light_2 = std::make_shared<PointLight>();
+    light_2->set_flux({1, 1, 1});
+    light_2->set_position({0, 0, 0});
+    light_2->set_n({0,1,0});
+
+    scene.add("floor", floor);
+    scene.add("wall_1", wall_1);
+    scene.add("wall_2", wall_2);
+    scene.add("wall_3", wall_3);
+ //   scene.add("triangle_2", square_front);
 
     scene.add("point light", light);
+    //scene.add("point light _2", light_2);
+
 
     info.screen_horizontal_size = info.screen_vertical_size = 600;
     info.horizontal_fow = info.vertical_fow = M_PI_2;
     
+    info.pos = {-10,3,5};
+
     info.beta = M_PI_2;
+    
    // info.alpha = 0;
     
     view_port.set(info);
